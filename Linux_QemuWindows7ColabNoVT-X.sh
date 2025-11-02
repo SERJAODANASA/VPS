21123e06eb719e619596035819089c7d4f68065a
SERJAODANASA <132838163+SERJAODANASA@users.noreply.github.com>
2025-11-02 12:24:29 -0300

    Update Linux_QemuWindows7ColabNoVT-X.sh

--- Linux_QemuWindows7ColabNoVT-X.sh
+++ Linux_QemuWindows7ColabNoVT-X.sh
@@ -1,32 +1,24 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
-./ngrok authtoken $CRP 
-nohup ./ngrok tcp 5900 &>/dev/null &
-echo Please wait for installing...
-echo "Installing QEMU (2-3m)..."
-sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
-echo Downloading Windows Disk...
-curl -L -o lite7.qcow2 https://app.vagrantup.com/daibangcam/boxes/windowsQCOW/versions/2.0/providers/qemu.box
-echo "Windows 7 x86 Lite On Google Colab"
-echo Your VNC IP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "Note: Use Right-Click Or Ctrl+C To Copy"
-echo "Please Keep Colab Tab Open, Maximum Time 12h"
-echo "Press F12, choose tab Console and paste these line then press Enter"
-echo ================================
-echo "function ClickConnect() {
-  console.log('Working')
-  document
-    .querySelector('#top-toolbar > colab-connect-button')
-    .shadowRoot.querySelector('#connect')
-    .click()
-} 
-setInterval(ClickConnect, 60000)"
-echo ================================
-echo Script by HanO
-echo Cell: +84978.39.41.43
-echo Email: daibangcam@gmail.com
-echo Website: hano.cf
-echo ================================
-sudo qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+#@title **Setup ngrok**
+
+
+#@markdown **Please enter your ngrok token here before running**
+#@markdown ****
+#@markdown It doesn't take too long
+import os
+YOURTOKEN = "" #@param {type:"string"}
+
+if YOURTOKEN == "34soqpPB3Nm7WeS5EnLInGNPUFC_2Vm3k1kAddKwmfQYPFZ2D" :
+            print("Please enter your token")
+elif len(str(YOURTOKEN)) < 49:
+   print("Your ngrok token is invalid. Please check back")
+elif len(str(YOURTOKEN)) > 49:
+   print("Your ngrok token is invalid. Please check back")
+else:
+  print("Installing ngrok...")
+  os.system("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
+  os.system("unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1")
+  os.system("rm -rf ngrok-stable-linux-amd64.zip")
+  print("Setting up...")
+  os.system(f"./ngrok authtoken {YOURTOKEN}")
+  os.system("nohup ./ngrok tcp --region ap 5900 &>/dev/null &")
+  print("Done")
